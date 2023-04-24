2fb84a01f0f13fa636049324e1f83353a5c2b557
solynx <alidoan2nd.solynx@gmail.com>
2023-04-25 00:16:23 +0700

    test1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,14 @@
 import { defineNuxtConfig } from 'nuxt/config'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-  app: {
-    baseURL: '/mock-app/' // baseURL: '/<repository>/'
+  mode: 'static',
+  router: {
+     base: '/mock-app/'
   },
-
   modules: [
     // ...
     '@pinia/nuxt',
   ],
-  
   ssr:false,
   
     css: ['~/assets/css/main.css'],
