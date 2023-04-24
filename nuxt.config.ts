7984b202cb91a2e714c2abc75bc426c56e399f7a
solynx <alidoan2nd.solynx@gmail.com>
2023-04-24 21:29:50 +0700

    Adding github pages deploy workflow

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 import { defineNuxtConfig } from 'nuxt/config'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  target: 'static',
+  router: {
+    base: "/mock-app/"
+  },
   app: {
     baseURL: '/mock-app/' // baseURL: '/<repository>/'
   },
