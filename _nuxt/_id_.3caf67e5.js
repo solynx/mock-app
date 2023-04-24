036893fd6c4753643db892d3e682bca8a152b6fa
solynx <alidoan2nd.solynx@gmail.com>
2023-04-24 23:47:21 +0700

    d661d8a

---
+++ _nuxt/_id_.3caf67e5.js
@@ -0,0 +1,1 @@
+import{a,o,b as t,t as e}from"./entry.1d3c1560.js";const s={};function n(r,c){return o(),t("p",null,e(r.$route.params.group)+" - "+e(r.$route.params.id),1)}const _=a(s,[["render",n]]);export{_ as default};
---
+++ _nuxt/test.b1b07082.js
@@ -0,0 +1,1 @@
+import{a as e,o as t,b as c}from"./entry.1d3c1560.js";const o={};function n(r,a){return t(),c("h1",null,"alo 123")}const _=e(o,[["render",n]]);export{_ as default};
